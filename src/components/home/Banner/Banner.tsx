1760e32469c20ca8d94ae2ec1aecc1d18630cb10
SujonRoyBD <sujonroy537527@gmail.com>
2025-08-01 10:17:52 +0600

    banner logo set

--- src/components/home/Banner/Banner.tsx
+++ src/components/home/Banner/Banner.tsx
@@ -1,5 +1,6 @@
+import Image from 'next/image';
 import React from 'react';
-
+import { GoArrowRight } from "react-icons/go";
 const Banner = () => {
     return (
         <div className='bg-[url("/assets/heroSection.png")] bg-cover bg-center text-white py-20 px-6'>
@@ -10,7 +11,14 @@ const Banner = () => {
                 <p className='text-lg md:text-xl leading-relaxed'>
                     At Sum Dim Sum, we bring families and friends together over the timeless tradition of sharing delectable dishes. From our expertly crafted dim sum to our vibrant ambiance, every moment here is a celebration of taste and togetherness. Explore our menu, reserve your table, or order your favorites online today!
                 </p>
+                <div className='py-16'>
+                    <button className='flex items-center justify-center mx-auto gap-[8px] bg-[#B31217] px-[20px] py-[12px] rounded-lg'>Explore Our Menu <GoArrowRight className=''/></button>
+                </div>
+             
             </div>
+               <div className='lg:flex justify-end items-end container'>
+                    <Image src="/assets/bannerLogo.png"alt='banner-logo' width={174} height={174} className='rounded-full'/>
+                </div>
         </div>
     );
 };
